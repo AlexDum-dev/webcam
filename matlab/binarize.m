13a5b9f1c16453203c63819f9fe2e5119a485833
AlexDum-dev <alexandre.dumarquez@gmail.com>
2020-05-26 14:34:29 +0200

    Inverse la fonction de binarisation

--- matlab/binarize.m
+++ matlab/binarize.m
@@ -2,10 +2,12 @@
 % return image_bin : image binarisée
 
 
-function image_bin = binarise(image)
+function image_finale = binarize(image)
 
 image_gray = rgb2gray(image); % Transforme l'image couleur par une image grayscale
 
-image_bin = imbinarize(image_gray); % Binarize l'image grayscale
+image_bin = imcomplement(imbinarize(image_gray, 0.7));% Binarize l'image grayscale
+
+image_finale = im2uint8(image_bin);
 
 end
